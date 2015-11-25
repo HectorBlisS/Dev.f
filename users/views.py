33c76c92c3e616b64af45de0deb849d8930ae19a
FixTeR Studios <BlisS@MacBook-Pro-de-FixTeR.local>
2015-11-24 21:22:09 -0600

    Se generó otra app, para mostrar datos de un diccionario

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+DB=[{
+	"id":1,
+	"nombre":"BliS",
+	"edad":28,
+
+},{
+	"id":2,
+	"nombre":"Gus",
+	"edad":29,
+},{
+	"id":3,
+	"nombre":"Viri",
+	"edad":31,
+}]
+
+
+def usuarios(request,nombre):
+	# return HttpResponse("Tu nombre es: "+nombre)
+	return render(request,'users/tuNombre.html', {"nombre":nombre})
+
+def dar_usuario(request,id):
+	for usr in DB:
+		if int(id)==usr["id"]:
+			return render(request,'users/tuNombre.html',{
+				"nombre":usr["nombre"],
+				"edad":usr["edad"]
+				})
+	return HttpResponse("<h1>Usuario no encontrado</h1>")
